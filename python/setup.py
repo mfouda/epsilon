a7be76ed9fdb0b4e56e2336d08c7bbe3ae946f80
Matt Wytock <mwytock@gmail.com>
2015-09-07 19:50:32 -0400

    tweaks to setup script

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,10 @@
+"""Setup script for epsilon.
+
+Epsilon depends on having protobuf >3.0.0a3 installed. We assume that `protoc`
+is avaiable on the command line and that the library/headers live in the same
+location.
+"""
+
 import os
 import subprocess
 import sys
@@ -15,6 +22,8 @@ PROTOC = find_executable("protoc")
 if PROTOC is None:
     std.stderr.write("protoc is not installed")
     sys.exit(-1)
+PROTOBUF_PREFIX = os.path.dirname(os.path.dirname(PROTOC))
+PROTOBUF_LIB = os.path.join(PROTOBUF_PREFIX, "lib")
 
 EPSILON_SOURCES = [
     "epsilon/algorithms/prox_admm.cc",
@@ -46,9 +55,22 @@ EPSILON_PROTO_SOURCES = [src.replace(".proto", ".pb.cc")
                          for src in EPSILON_PROTOS]
 
 
-def generate_proto(source, out_flag):
-    print "Compiling %s..." % source
+PROTO_PY = 0
+PROTO_CC = 1
+def generate_proto(source, output):
     source_path = os.path.join(PROTO_DIR, source)
+    if output == PROTO_PY:
+        out_path = source.replace(".proto", "_pb2.py")
+        out_flag = "--python_out=."
+    else:
+        out_path = source.replace(".proto", "pb.cc")
+        out_flag = "--cpp_out=."
+
+    if (os.path.isfile(out_path) and
+        os.path.getmtime(out_path) > os.path.getmtime(source_path)):
+        return
+
+    print "Generating %s..." % out_path
     cmd = [PROTOC, "-I", PROTO_DIR, source_path, out_flag]
     if subprocess.call(cmd) != 0:
         sys.exit(-1)
@@ -56,13 +78,13 @@ def generate_proto(source, out_flag):
 class build_py(_build_py):
     def run(self):
         for proto in EPSILON_PROTOS:
-            generate_proto(proto, "--python_out=.")
+            generate_proto(proto, PROTO_PY)
         _build_py.run(self)
 
 class build_ext(_build_ext):
     def run(self):
         for proto in EPSILON_PROTOS:
-            generate_proto(proto, "--cpp_out=.")
+            generate_proto(proto, PROTO_CC)
         _build_ext.run(self)
 
 solve = Extension(
@@ -76,13 +98,13 @@ solve = Extension(
     include_dirs = [
         SOURCE_DIR,
         EIGEN_DIR,
-        "/usr/local/include",
+        PROTOBUF_LIB,
         ".",
     ],
-    libraries = ["protobuf"],
     library_dirs = [
-        "/usr/local/lib",
-    ]
+        PROTOBUF_LIB
+    ],
+    libraries = ["protobuf"],
 )
 
 setup(
