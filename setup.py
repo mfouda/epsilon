0e4eae90980bded3b68cc3f88ccd5d036cee9367
Matt Wytock <mwytock@gmail.com>
2015-09-07 20:55:28 -0400

    mv setup.py to top level directory for easier packaging

--- setup.py
+++ setup.py
@@ -11,12 +11,14 @@ import sys
 
 from distutils.command.build_ext import build_ext as _build_ext
 from distutils.command.build_py import build_py as _build_py
+from distutils.dep_util import newer
 from distutils.spawn import find_executable
+from distutils import log
 from setuptools import find_packages, setup, Extension
 
-SOURCE_DIR = "../src"
-PROTO_DIR = "../proto"
-EIGEN_DIR = "../third_party/eigen"
+SOURCE_DIR = "src"
+PROTO_DIR = "proto"
+EIGEN_DIR = "third_party/eigen"
 
 PROTOC = find_executable("protoc")
 if PROTOC is None:
@@ -40,8 +42,7 @@ EPSILON_SOURCES = [
     "epsilon/util/vector.cc",
     "epsilon/util/vector_file.cc",
 ]
-EPSILON_SOURCES = [os.path.abspath(SOURCE_DIR + "/" + src)
-                   for src in EPSILON_SOURCES]
+EPSILON_SOURCES = [SOURCE_DIR + "/" + src for src in EPSILON_SOURCES]
 
 EPSILON_PROTOS = [
     "epsilon/data.proto",
@@ -51,55 +52,71 @@ EPSILON_PROTOS = [
     "epsilon/stats.proto",
     "epsilon/status.proto",
 ]
-EPSILON_PROTO_SOURCES = [src.replace(".proto", ".pb.cc")
-                         for src in EPSILON_PROTOS]
-
 
 PROTO_PY = 0
 PROTO_CC = 1
-def generate_proto(source, output):
-    source_path = os.path.join(PROTO_DIR, source)
-    if output == PROTO_PY:
-        out_path = source.replace(".proto", "_pb2.py")
-        out_flag = "--python_out=."
+def generate_proto(src_name, format, dst_dir, verbose):
+    src = os.path.join(PROTO_DIR, src_name)
+    if format == PROTO_PY:
+        dst = os.path.join(dst_dir, src_name.replace(".proto", "_pb2.py"))
+        out_flag = "--python_out=" + dst_dir
     else:
-        out_path = source.replace(".proto", "pb.cc")
-        out_flag = "--cpp_out=."
+        dst = os.path.join(dst_dir, src_name.replace(".proto", ".pb.cc"))
+        out_flag = "--cpp_out=" + dst_dir
 
-    if (os.path.isfile(out_path) and
-        os.path.getmtime(out_path) > os.path.getmtime(source_path)):
-        return
+    if newer(src, dst):
+        if verbose >= 1:
+            log.debug("generating %s...", dst)
 
-    print "Generating %s..." % out_path
-    cmd = [PROTOC, "-I", PROTO_DIR, source_path, out_flag]
-    if subprocess.call(cmd) != 0:
-        sys.exit(-1)
+        cmd = [PROTOC, "-I", PROTO_DIR, src, out_flag]
+        if subprocess.call(cmd) != 0:
+            sys.exit(-1)
+
+    return dst
 
 class build_py(_build_py):
     def run(self):
+        self.mkpath(self.build_lib)
         for proto in EPSILON_PROTOS:
-            generate_proto(proto, PROTO_PY)
+            generate_proto(proto, PROTO_PY, self.build_lib, self.verbose)
         _build_py.run(self)
 
 class build_ext(_build_ext):
     def run(self):
-        for proto in EPSILON_PROTOS:
-            generate_proto(proto, PROTO_CC)
+        for ext in self.extensions:
+            ext.sources = self.gen_proto_sources(ext.sources, ext)
         _build_ext.run(self)
+
+    def gen_proto_sources(self, sources, ext):
+        new_sources = []
+        has_proto = False
+        for source in sources:
+            if os.path.splitext(source)[1] == ".proto":
+                has_proto = True
+                self.mkpath(self.build_temp)
+                new_sources.append(
+                    generate_proto(
+                        source, PROTO_CC, self.build_temp, self.verbose))
+            else:
+                new_sources.append(source)
+
+        if has_proto:
+            ext.include_dirs.append(self.build_temp)
+
+        return new_sources
 
 solve = Extension(
     "epsilon._solve",
     language = "c++",
     sources = (
-        EPSILON_PROTO_SOURCES +
+        EPSILON_PROTOS +
         EPSILON_SOURCES +
-        ["epsilon/solvemodule.cc"]),
+        ["python/epsilon/solvemodule.cc"]),
     extra_compile_args = ["-std=c++14"],
     include_dirs = [
         SOURCE_DIR,
         EIGEN_DIR,
         PROTOBUF_LIB,
-        ".",
     ],
     library_dirs = [
         PROTOBUF_LIB
@@ -112,7 +129,8 @@ setup(
     version = "1.0a1",
     author = "Matt Wytock",
     author_email = "mwytock@gmail.com",
-    packages = find_packages(),
+    packages = find_packages("python"),
+    package_dir = {"epsilon": "python/epsilon"},
     ext_modules = [solve],
     install_requires = [
         "cvxpy==0.2.28",
