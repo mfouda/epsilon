248b3be988d2a350da21c97678b23bbb9c8e5b97
Matt Wytock <mwytock@gmail.com>
2015-09-08 22:45:59 -0400

    add test for solving lasso

--- python/epsilon/test_solve.py
+++ python/epsilon/test_solve.py
@@ -1,22 +1,39 @@
 
-
-import unittest
-
 import cvxpy as cp
 import numpy as np
 
 from epsilon import solve
+from epsilon import status_pb2
 
-class TestSolve(unittest.TestCase):
-    def test_lasso(self):
-        m = 5
-        n = 10
+def get_values(prob):
+    values = {}
+    for var in prob.variables():
+        values[var.id] = var.value
+    return values
 
-        np.random.seed(0)
-        A = np.random.randn(m,n)
-        b = np.random.randn(m,1)
-        x = cp.Variable(n,1)
-        lam = 1
+def assert_values_equal(a, b, tol=1e-2):
+    assert a.keys() == b.keys()
+    for var_id in a.keys():
+        np.testing.assert_allclose(a[var_id], b[var_id], atol=tol)
 
-        f = 0.5*cp.sum_squares(A*x - b) + lam*cp.norm1(x);
-        status = solve.solve(cp.Problem(cp.Minimize(f)))
+def test_lasso():
+    m = 5
+    n = 10
+
+    np.random.seed(0)
+    A = np.random.randn(m,n)
+    b = np.random.randn(m,1)
+    x = cp.Variable(n,1)
+    lam = 1
+
+    f = 0.5*cp.sum_squares(A*x - b) + lam*cp.norm1(x)
+    prob = cp.Problem(cp.Minimize(f))
+
+    status = solve.solve(prob)
+    assert status.state == status_pb2.ProblemStatus.OPTIMAL
+    values0 = get_values(prob)
+
+    prob.solve()
+    values1 = get_values(prob)
+
+    assert_values_equal(values0, values1)
