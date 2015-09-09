9765cfbeab036ef5525e441d90f5428a388cf1e3
Matt Wytock <mwytock@gmail.com>
2015-09-08 23:52:19 -0400

    add travis.yml and install script

---
+++ tools/install_travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+#
+# Script to run tests on Travis CI
+
+# Numpy/scipy environment via conda
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
+conda install --yes python=2.7 pip pip numpy scipy
+pip install cvxpy
+
+# Environment information
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import scipy; print('cvxpy %s' % cvxpy.__version__)"
+
+# Install epsilon
+cd python
+python setup.py install
