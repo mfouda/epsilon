1a23ed684ddd9729a2a088dee1227037f87505a7
Matt Wytock <mwytock@gmail.com>
2015-09-09 00:11:02 -0400

    tweak tweak travis CI

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -1,8 +1,6 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 #
 # Script to run tests on Travis CI
-
-lsb_release -a
 
 # Numpy/scipy environment via conda
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
@@ -11,13 +9,15 @@ chmod +x miniconda.sh
 export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
 conda install --yes python=2.7 pip atlas numpy scipy nose
-pip install cvxpy
 
 # Environment information
+ls -l /home/travis/miniconda/lib
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
-python -c "import scipy; print('cvxpy %s' % cvxpy.__version__)"
+
+# Install cvxpy
+pip install cvxpy
 
 # Install epsilon
 cd python
