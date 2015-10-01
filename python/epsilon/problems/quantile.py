ebbb32d02b785cbf13210fc48f22b817ed45ed10
Matt Wytock <mwytock@gmail.com>
2015-10-01 01:02:50 -0400

    working version of multiple quantile regression

--- python/epsilon/problems/quantile.py
+++ python/epsilon/problems/quantile.py
@@ -2,24 +2,26 @@ import cvxpy as cp
 import numpy as np
 import scipy.sparse as sp
 
+def quantile_loss(x, alpha):
+    return cp.sum_entries(cp.max_elemwise(-alpha*x, (1-alpha)*x))
+
 def create(m, n, k):
     np.random.seed(0)
 
     # Generate data
     x = np.random.rand(m)*2*np.pi - np.pi
     y = np.sin(x) + 0.1*(x+np.pi)*np.random.randn(m)
-    alphas = np.linspace(1./k, 1-1./k, k-1)
+    alphas = np.linspace(1./(k+1), 1-1./(k+1), k)
 
     # RBF features
     mu_rbf = np.array([np.linspace(-np.pi-1, np.pi+1, n)])
     mu_sig = np.median(np.sqrt((mu_rbf.T - mu_rbf)**2))
     X = np.hstack([np.exp(-(mu_rbf.T - x).T**2/(2*mu_sig**2)), np.ones((m,1))])
 
-    quantile_loss = lambda x, alpha : cp.sum_entries(cp.max_elemwise(-alpha*x, (1-alpha)*x))
-    thetas = [cp.Variable(n+1) for i in xrange(len(alphas))]
-    f = sum([quantile_loss(X*thetas[i] - y, alpha) for i, alpha in enumerate(alphas)])
+    Theta = cp.Variable(n+1,k)
+    XT = cp.Variable(m,k)
+    f = sum([quantile_loss(XT[:,i] - y, alphas[i]) for i in xrange(k)])
+    c = [XT == X*Theta,
+         XT[:,1:] - XT[:,:-1] >= 0]
 
-    #DXT = X*(Theta[:,1:] - Theta[:,:-1])
-    #C = [DXT >= 0]
-
-    return cp.Problem(cp.Minimize(f))
+    return cp.Problem(cp.Minimize(f), c)
