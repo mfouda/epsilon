33c13a8247c063767f564a080ff64ec4eb38c735
Matt Wytock <mwytock@gmail.com>
2015-10-22 12:41:12 -0400

    beginning of linear_map type in compiler

---
+++ python/epsilon/linear_map.py
@@ -0,0 +1,42 @@
+
+# Atomic linear maps
+def kron(A, B):
+    pass
+
+def dense(C):
+    pass
+
+def sparse(C):
+    pass
+
+def diagonal(c):
+    pass
+
+def scalar(alpha, n):
+    pass
+
+
+# Implementation of various linear maps in terms of atoms
+def identity(n):
+    pass
+
+def index(n):
+    pass
+
+def transpose(m, n, A):
+    pass
+
+def one_hot(i, n):
+    pass
+
+def sum(n):
+    pass
+
+def negate(n):
+    pass
+
+def matrix_product_left(A, n):
+    pass
+
+def matrix_product_right(B, m):
+    pass
