0d94cda4bb3cc183fe6ab61fc044e5dc3fd15656
Matt Wytock <mwytock@gmail.com>
2015-10-23 11:11:57 -0400

    library of linear maps

--- python/epsilon/compiler/canonicalize_linear.py
+++ python/epsilon/compiler/canonicalize_linear.py
@@ -1,29 +1,14 @@
 """Implements the linear canonicalize transforms on the AST."""
 
-
-# Helper functions
-# TODO(mwytock): Move to cannicalize_util.py
-def is_constant(expr):
-    pass
-
-def is_linear(expr):
-    pass
-
-def only_arg(expr):
-    if len(expr.arg) != 1:
-        raise CanonicalizeError("wrong number of args", expr)
-    return expr.arg[0]
-
-def dim(expr, index=None):
-    if len(expr.size.dim) != 2:
-        raise CanonicalizeError("wrong number of dimensions", expr)
-    if index is None:
-        return expr.size.dim[0]*expr.size.dim[1]
-    else:
-        return expr.size.dim[index]
+from epsilon import expression
+from epsilon import linear_map
+from epsilon.compiler.canonicalize_util import *
 
 # Transforms on the AST
 def transform_variable(expr):
+    return expr
+
+def transform_constant(expr):
     return expr
 
 def transform_add(expr):
@@ -32,47 +17,68 @@ def transform_add(expr):
 def transform_transpose(expr):
     return expression.linear_map(
         linear_map.transpose(dim(expr,0), dim(expr,1)),
-        only_arg(expr))
+        transform_expr(only_arg(expr)))
 
 def transform_index(expr):
     return expression.linear_map(
         linear_map.kronecker_product(
             linear_map.index(expr.key[1], dim(only_arg(expr),1)),
-            linear_map.index(expr.key[0], dim(only_arg(expr),0))))
+            linear_map.index(expr.key[0], dim(only_arg(expr),0))),
+        transform_expr(only_arg(expr)))
 
-def transform_multiply_generic(expr, const_transform):
+def transform_multiply_generic(expr, f_const):
     if len(expr.arg) != 2:
         raise CanonicalizeError("wrong number of args", expr)
 
-    if is_constant(expr.arg(0)):
+    if expr.arg[0].curvature.curvature_type == Curvature.CONSTANT:
         return expression.linear_map(
-            linear_map.left_matrix_product(...),
-            expr.arg(1))
-    elif is_constant(expr.arg(1)):
+            linear_map.left_matrix_product(
+                f_const(transform_expr(expr.arg[0])),
+                ...),
+            transform_expr(expr.arg(1)))
+    elif expr.arg[1].curvature.curvature_type == Curvature.CONSTANT:
         return expression.linear_map(
-            linear_map.right_matrix_product(...),
-            expr.arg(0))
+            linear_map.right_matrix_product(
+                f_const(transform_expr(expr.arg(1))),
+                ...),
+            transform_expr(expr.arg(0)))
 
     raise CanonicalizeError("multiplying two non constants", expr)
 
+def constant_multiply(expr):
+    leaf = only_leaf(expr)
+    leaf.CopyFrom(
+        expression.linear_map(linear_map.dense(leaf.constant))
+    return expr
+
 def transform_multiply(expr):
-    return transform_multiply_generic(expr, linear_map.xx())
+    return transform_multiply_generic(expr, constant_multiply)
+
+def constant_multiply_elementwise(expr):
+    leaf = only_leaf(expr)
+    leaf.CopyFrom(
+        expression.linear_map(linear_map.diagonal(leaf.constant)))
+    return expr
 
 def transform_multiply_elementwise(expr):
-    return transform_multiply_generic(expr, linear_map.yy())
+    return transform_multiply_generic(expr, constant_multiply_elementwise)
 
 def transform_negate(expr):
-    return expression.linear_map(linear_map.negate(dim(expr)), only_arg(expr))
+    return expression.linear_map(
+        linear_map.negate(dim(expr)),
+        transform_expr(only_arg(expr)))
 
 def transform_sum(expr):
-    return expression.linear_map(linear_map.sum(dim(expr)), only_arg(expr))
+    return expression.linear_map(
+        linear_map.sum(dim(expr)),
+        transform_expr(only_arg(expr)))
 
 def transform_linear_expr(expr):
     transform = locals()["transform_" + expr.expression_type().lower()]
     return transform(expr)
 
 def transform_expr(expr):
-    if is_linear(expr):
+    if expr.curvature.curvature_type == Curvature.AFFINE:
         return transform_linear_expr(expr)
     else:
         for arg in expr.arg:
--- python/epsilon/linear_map.py
+++ python/epsilon/linear_map.py
@@ -1,42 +1,83 @@
+"""Operations on LinearMaps."""
+
+import scipy.sparse as sp
+
+from epsilon.expression_pb2 import LinearMap
+from epsilon import constants
 
 # Atomic linear maps
-def kron(A, B):
-    pass
+def kronecker_product(A, B):
+    return LinearMap(
+        linear_map_type=LinearMap.KRONECKER_PRODUCT,
+        m=A.m*B.m,
+        n=A.n*B.n,
+        A=A, B=B)
 
-def dense(C):
-    pass
+def dense_matrix(constant, m, n):
+    return LinearMap(
+        linear_map_type=LinearMap.DENSE_MATRIX,
+        m=m,
+        n=n,
+        constant=constant)
 
-def sparse(C):
-    pass
+def sparse_matrix(constant, m, n):
+    return LinearMap(
+        linear_map_type=LinearMap.SPARSE_MATRIX,
+        m=m,
+        n=n,
+        constant=constant)
 
-def diagonal(c):
-    pass
+def diagonal_matrix(constant, n):
+    return LinearMap(
+        linear_map_type=LinearMap.DIAGONAL_MATRIX,
+        m=n,
+        n=n,
+        constant=constant)
 
 def scalar(alpha, n):
-    pass
+    return LinearMap(
+        linear_map_type=LinearMap.SCALAR,
+        m=n,
+        n=n,
+        scalar=alpha)
 
+# Operations on linear maps
+def transpose(A):
+    return LinearMap(
+        linear_map_type=LinearMap.TRANSPOSE,
+        m=A.n,
+        n=A.m,
+        A=A)
 
 # Implementation of various linear maps in terms of atoms
 def identity(n):
-    pass
+    return scalar(1, n)
 
-def index(n):
-    pass
-
-def transpose(m, n, A):
-    pass
+def index(slice, n):
+    m = slice.stop - slice.start
+    A = sp.coo_matrix(
+        np.ones(n),
+        (np.arange(m), np.arange(slice.start, m, slice.step)))
+    return sparse_matrix(constants.store(A), A.m, A.n)
 
 def one_hot(i, n):
-    pass
+    """[0, ... 0, 1, 0, ...]."""
+    a = sp.coo_matrix(([1], ([0], [i])), shape=(1, n))
+    return sparse_matrix(constants.store(a), 1, A.n)
 
 def sum(n):
-    pass
+    """All ones vector"""
+    return dense_matrix(constants.store(np.ones(n)), 1, A.n)
 
 def negate(n):
-    pass
+    return scalar(-1,n)
 
-def matrix_product_left(A, n):
-    pass
+def left_matrix_product(A, n):
+    if n==1:
+        return A
+    return kronecker_product(identity(n), A)
 
-def matrix_product_right(B, m):
-    pass
+def right_matrix_product(B, m):
+    if m==1:
+        return transpose(B)
+    return kronecker_product(transpose(B), identity(m))
