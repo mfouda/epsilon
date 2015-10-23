2e441d3eec681c32002d4eaf3fa1f31553e74b86
Matt Wytock <mwytock@gmail.com>
2015-10-23 13:08:21 -0400

    update data.py for sparse matrices

--- python/epsilon/data.py
+++ python/epsilon/data.py
@@ -5,11 +5,11 @@ import scipy.sparse as sp
 from epsilon.data_pb2 import Data
 from epsilon import expression
 
+# Global store of all constants
+data_map = {}
+
 METADATA_FILE = "metadata"
 VALUE_FILE = "value"
-
-data_map = {}
-expr_map = {}
 
 def metadata_file(prefix):
     return prefix + "/" + METADATA_FILE
@@ -18,36 +18,37 @@ def value_file(prefix):
     return prefix + "/" + VALUE_FILE
 
 def value_location(value):
-    # TODO(mwytock): Better hash function here? Some matrices may have same
-    # tostring() but different values.
-    return "/mem/data/" + str(abs(hash(value.tostring())))
+    return "/mem/data/" + str(abs(hash(value)))
 
-def store_ndarray(value, m, n, prefix):
-    data_map[metadata_file(prefix)] = Data(
-        data_type=Data.DENSE_MATRIX, m=m, n=n).SerializeToString()
-    data_map[value_file(prefix)] = value.tobytes(order="F")
+def value_data(value):
+    # TODO(mwytock): Need to ensure type double/float here?
+    if isinstance(value, np.ndarray):
+        metadata = Data(
+            data_type=Data.DENSE_MATRIX,
+            m=value.shape[0],
+            n=1 if len(value.shape) == 1 else value.shape[1])
+        value_bytes = value.tobytes(order="F")
 
-def store_coo_matrix(value, m, n, prefix):
-    data_map[metadata_file(prefix)] = Data(
-        data_type=Data.SPARSE_MATRIX, m=m, n=n).SerializeToString()
-    data_map[value_file(prefix)] = 0
+    elif isinstance(value, sp.spmatrix):
+        csc = value.tocsc()
+        metadata = Data(
+            data_type=Data.SPARSE_MATRIX,
+            m=value.shape[0],
+            n=1 if len(value.shape) == 1 else value.shape[1],
+            nnz=value.nnz)
+        value_bytes = (csc.data.tobytes("F") +
+                       csc.indices.tobytes("F") +
+                       csc.indptr.tobytes("F"))
+
+    else:
+        raise ValueError("unknown value type " + str(value))
+
+    return metadata, value_bytes
 
 def store_constant(value):
-    prefix = value_location(value)
-
-    expr = expr_map.get(prefix, None)
-    if not expr:
-        m = value.shape[0]
-        n = 1 if len(value.shape) == 1 else value.shape[1]
-
-        if isinstance(value, np.ndarray):
-             store_ndarray(value, m, n, prefix)
-        elif isinstance(value, sp.coo_matrix):
-             store_coo_matrix(value, m, n, prefix)
-        else:
-            raise ValueError("unknown value: " + str(value))
-
-        expr = expression.constant(m, n, data_location=prefix)
-        expr_map[prefix] = expr
-
-    return expr
+    metadata, value_bytes = value_data(value)
+    prefix = value_location(value_bytes)
+    data_map[metadata_file(prefix)] = metadata.SerializeToString()
+    data_map[value_file(prefix)] = value_bytes
+    return expression.constant(
+        metadata.m, metadata.m, data_location=prefix)
