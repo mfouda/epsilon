5824f27879e5d5e8c94a49ccfa16420b9cb1c1ef
Matt Wytock <mwytock@gmail.com>
2015-10-30 03:20:36 +0000

    update comment

--- tools/run_benchmarks.sh
+++ tools/run_benchmarks.sh
@@ -5,8 +5,8 @@
 
 benchmark="python -m epsilon.problems.benchmark"
 
-# Epsilon is not multi-threaded (yet) but numpy BLAS implementations
-# can be.  
+# Epsilon is not multi-threaded (yet) but BLAS implementations used as a
+# backend to numpy can be.
 export OPENBLAS_NUM_THREADS=1
 
 parallel python -m epsilon.problems.benchmark \
