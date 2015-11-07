4214dc46f1153777f22a56fd13af632b9aab66c7
Matt Wytock <mwytock@gmail.com>
2015-11-07 03:34:18 +0000

    small changes to run_benchmarks_serial

--- tools/run_benchmarks_serial.sh
+++ tools/run_benchmarks_serial.sh
@@ -2,22 +2,20 @@
 #
 # Run Epsilon benchmarks using GNU parallel. 
 
-benchmark="python -m epsilon.problems.benchmark"
+cmd="python -m epsilon.problems.benchmark $*"
 
-if [ $# -eq 1 ]; then
-    names=$1
-else
-    names=$($benchmark --list-benchmarks)
-fi
-problems=$($benchmark --list-problems)
+benchmarks=$($cmd --list-benchmarks)
+problems=$($cmd --list-problems)
 time=3600
+
+echo $problems
 
 # Epsilon is not multi-threaded (yet) but BLAS implementations used as a
 # backend to numpy can be.
 export OPENBLAS_NUM_THREADS=1
 
 for problem in $problems; do
-    for name in $names; do
-	timelimit -t$time $benchmark --benchmark $name --problem $problem
+    for benchmark in $benchmarks; do
+	timelimit -t$time $cmd --benchmark $benchmark --problem $problem
     done
 done
