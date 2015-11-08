67c749e213f98f786016259a06cf0bf0e8f92fe2
Matt Wytock <mwytock@gmail.com>
2015-11-07 21:16:15 -0500

    change lambda for hinge_l1

--- python/epsilon/problems/hinge_l1.py
+++ python/epsilon/problems/hinge_l1.py
@@ -6,14 +6,11 @@ from epsilon.problems import problem_util
 
 def create(**kwargs):
     A, b = problem_util.create_classification(**kwargs)
-
-    # TODO(mwytock): This is based on lambda_max for logreg_l1, find the right
-    # rule here.
-    ratio = float(np.sum(b==1)) / len(b)
-    lambda_max = np.abs((1-ratio)*A[b==1,:].sum(axis=0) +
-                        ratio*A[b==-1,:].sum(axis=0)).max()
-    lam = 0.9*lambda_max
+    m = kwargs["m"]
+    n = kwargs["n"]
+    sigma = 0.05
+    lam = sigma*np.sqrt(m*np.log(n))
 
     x = cp.Variable(A.shape[1])
-    f = problem_util.hinge(1-sp.diags([b],[0])*A*x) + lam*cp.norm1(x)
+    f =  problem_util.hinge(1-sp.diags([b],[0])*A*x) + lam*cp.norm1(x)
     return cp.Problem(cp.Minimize(f))
