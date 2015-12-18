cf60fcdf290e8793c55f306a391cdb261e0ada74
Matt Wytock <mwytock@gmail.com>
2015-12-18 11:58:41 -0800

    add poisson_loss and softmax_loss along with tests

--- python/epopt/functions.py
+++ python/epopt/functions.py
@@ -14,8 +14,20 @@ def quantile_loss(alphas, Theta, X, y):
     k = len(alphas)
     Y = np.tile(y, (k, 1)).T
     A = np.tile(alphas, (m, 1))
-    XT = X*Theta
+    Z = X*Theta - Y
     return cp.sum_entries(
         cp.max_elemwise(
-            cp.mul_elemwise( -A, XT - Y),
-            cp.mul_elemwise(1-A, XT - Y)))
+            cp.mul_elemwise( -A, Z),
+            cp.mul_elemwise(1-A, Z)))
+
+def softmax_loss(Theta, X, y):
+    m = len(y)
+    n, k = Theta.size
+    Y = sp.coo_matrix((np.ones(m), (np.arange(m), y)), shape=(m, k))
+    print cp.__file__
+    return (cp.sum_entries(cp.mul_elemwise(Y, X*Theta)) -
+            cp.sum_entries(cp.log_sum_exp(X*Theta, axis=1)))
+
+def poisson_loss(theta, X, y):
+    return (cp.sum_entries(cp.exp(X*theta)) -
+            cp.sum_entries(sp.diags([y],[0])*X*theta))
