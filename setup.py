a91f3b883d04eca3a0b632958407eccfc3e3964b
Matt Wytock <mwytock@gmail.com>
2016-01-07 14:16:32 -0800

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ else:
 
 setup(
     name = "epopt",
-    version = "0.2.2",
+    version = "0.2.3",
     author = "Matt Wytock",
     url = "http://epopt.io/",
     author_email = "mwytock@gmail.com",
