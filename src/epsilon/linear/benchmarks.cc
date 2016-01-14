070519eb1eb85b58f98098bb3f7c3c56733200a0
Matt Wytock <mwytock@gmail.com>
2016-01-14 15:36:17 -0800

    minor tweaks to benchmark

--- src/epsilon/linear/benchmarks.cc
+++ src/epsilon/linear/benchmarks.cc
@@ -3,6 +3,7 @@
 #include <benchmark/benchmark.h>
 
 #include <Eigen/SparseCholesky>
+#include <Eigen/Cholesky>
 
 // void BM_Sparse_LDLT_Identity(benchmark::State& state) {
 //   const int n = state.range_x();
@@ -17,14 +18,24 @@
 // }
 // BENCHMARK(BM_Sparse_LDLT_Identity)->Range(2<<10, 2<<15);
 
-void BM_DenseMatrixMultiply(benchmark::State& state) {
-  LOG(INFO) << "threads: " << Eigen::nbThreads();
+// void BM_DenseMatrixMultiply(benchmark::State& state) {
+//   srand(0);
+//   Eigen::MatrixXd A = Eigen::MatrixXd::Random(10000, 3000);
+//   while (state.KeepRunning()) {
+//     Eigen::MatrixXd ATA = A.transpose()*A;
+//   }
+// }
+// BENCHMARK(BM_DenseMatrixMultiply);
+
+void BM_DenseCholesky(benchmark::State& state) {
   srand(0);
-  Eigen::MatrixXd A = Eigen::MatrixXd::Random(10000, 3000);
+  Eigen::MatrixXd A = Eigen::MatrixXd::Random(3000, 3000);
+  Eigen::MatrixXd ATA = A.transpose()*A;
   while (state.KeepRunning()) {
-    Eigen::MatrixXd ATA = A.transpose()*A;
+    Eigen::LLT<Eigen::MatrixXd> llt;
+    llt.compute(ATA);
   }
 }
-BENCHMARK(BM_DenseMatrixMultiply);
+BENCHMARK(BM_DenseCholesky);
 
 BENCHMARK_MAIN();
