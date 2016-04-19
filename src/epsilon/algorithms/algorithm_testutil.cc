31f4654e8fbc210e36427b6d8f423816a2379f12
Matt Wytock <mwytock@gmail.com>
2016-04-19 11:02:19 -0700

    remove one more missing dep

--- src/epsilon/algorithms/algorithm_testutil.cc
+++ src/epsilon/algorithms/algorithm_testutil.cc
@@ -1,6 +1,5 @@
 
 #include "epsilon/algorithms/algorithm_testutil.h"
-#include "epsilon/expression/expression_testutil.h"
 
 Eigen::VectorXd ComputeLS(const Eigen::MatrixXd& A, const Eigen::VectorXd& b) {
   Eigen::LLT<Eigen::MatrixXd> solver;
