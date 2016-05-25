58249f98267051d060bc9ca27636bbad50583e99
Po-Wei Wang <xflash96@gmail.com>
2016-05-25 18:01:44 -0400

    update benchmark for max_softmax

--- python/epopt/problems/max_softmax.py
+++ python/epopt/problems/max_softmax.py
@@ -11,24 +11,22 @@ def create(**kwargs):
     m = kwargs['m']  #instance
     n = kwargs['n']  #dim
     p = 5   #p-largest
-    q = 10
     X = problem_util.normalized_data_matrix(m,n,1)
-    Y = np.random.randint(0, k-1, (q,m))
+    Y = np.random.randint(0, k, m)
 
     Theta = cp.Variable(n,k)
-    t = cp.Variable(q)
+    t = cp.Variable(1)
     texp = cp.Variable(m)
-    f = cp.sum_largest(t, p)+cp.sum_entries(texp) + cp.sum_squares(Theta)
+    f = t+cp.sum_largest(texp, p) + cp.sum_squares(Theta)
     C = []
     C.append(cp.log_sum_exp(X*Theta, axis=1) <= texp)
-    for i in range(q):
-        Yi = one_hot(Y[i], k)
-        C.append(-cp.sum_entries(cp.mul_elemwise(X.T.dot(Yi), Theta)) == t[i])
+    Yi = one_hot(Y, k)
+    C.append(-cp.sum_entries(cp.mul_elemwise(X.T.dot(Yi), Theta)) == t)
 
-    t_eval = lambda: np.array([
-        -cp.sum_entries(cp.mul_elemwise(X.T.dot(one_hot(Y[i], k)), Theta)).value for i in range(q)])
-    f_eval = lambda: cp.sum_largest(t_eval(), p).value \
-        + cp.sum_entries(cp.log_sum_exp(X*Theta, axis=1)).value \
+    t_eval = lambda: \
+        -cp.sum_entries(cp.mul_elemwise(X.T.dot(one_hot(Y, k)), Theta)).value 
+    f_eval = lambda: t_eval() \
+        + cp.sum_largest(cp.log_sum_exp(X*Theta, axis=1), p).value \
         + cp.sum_squares(Theta).value
     
     return cp.Problem(cp.Minimize(f), C), f_eval
